4459a442dddf1e72a7b3ceff808fd595240d7e05
Kimminsu <alstn2468_@naver.com>
2020-07-16 11:57:16 +0900

    [#22] Fix product list action, reducer setting

--- src/actions/productListAction.ts
+++ src/actions/productListAction.ts
@@ -1,10 +1,28 @@
-import { createAction, ActionType } from 'typesafe-actions';
+import { createAction, createAsyncAction, ActionType } from 'typesafe-actions';
+import { ProductItemType } from '../components/Product/ProductType';
 
-export const FETCH_PRODUCT_LIST = 'FETCH_PRODUCT_LIST';
+export const FETCH_PRODUCT_LIST_REQUEST = 'FETCH_PRODUCT_LIST_REQUEST';
+export const FETCH_PRODUCT_LIST_SUCCESS = 'FETCH_PRODUCT_LIST_SUCCESS';
+export const FETCH_PRODUCT_LIST_FAILURE = 'FETCH_PRODUCT_LIST_FAILURE';
 export const CHANGE_PRODUCT_LIST_CURRENT_PAGE =
     'CHANGE_PRODUCT_LIST_CURRENT_PAGE';
 
-export const fetchProductList = createAction(FETCH_PRODUCT_LIST)();
+/* ================================== *
+ * 상품 목록을 가져오는 비동기 액션   *
+ * TRequestPayload : 페이지 번호      *
+ * TSuccessPayload : 상품 목록 리스트 *
+ * TFailurePayload : 실패 메시지      *
+ * ================================== */
+export const fetchProductList = createAsyncAction(
+    FETCH_PRODUCT_LIST_REQUEST,
+    FETCH_PRODUCT_LIST_SUCCESS,
+    FETCH_PRODUCT_LIST_FAILURE,
+)<number, ProductItemType[], string>();
+
+/* ============================== *
+ * 현재 페이지 번호를 바꾸는 액션 *
+ * TPayload : 페이지 번호         *
+ * ============================== */
 export const changeProductListCurrentPage = createAction(
     CHANGE_PRODUCT_LIST_CURRENT_PAGE,
 )<number>();
--- src/reducers/productListReducer.ts
+++ src/reducers/productListReducer.ts
@@ -1,24 +1,31 @@
 import {
-    FETCH_PRODUCT_LIST,
     CHANGE_PRODUCT_LIST_CURRENT_PAGE,
+    FETCH_PRODUCT_LIST_REQUEST,
     ProductListAction,
 } from '../actions/productListAction';
+import { ProductItemType } from '../components/Product/ProductType';
 import { createReducer } from 'typesafe-actions';
 
 type ProductListState = {
     isFetching: boolean;
     currentPage: number;
+    productItems: ProductItemType[];
+    itemCounts: number;
 };
 
 const initialState: ProductListState = {
     isFetching: false,
     currentPage: 1,
+    productItems: [],
+    itemCounts: 0,
 };
+
+const PAGE_PER_COUNT: number = 5;
 
 const productListReducer = createReducer<ProductListState, ProductListAction>(
     initialState,
     {
-        [FETCH_PRODUCT_LIST]: (state) => ({ ...state }),
+        [FETCH_PRODUCT_LIST_REQUEST]: (state) => ({ ...state }),
         [CHANGE_PRODUCT_LIST_CURRENT_PAGE]: (state) => ({ ...state }),
     },
 );
