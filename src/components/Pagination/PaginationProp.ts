a7704840efedbe91498fc859edc7f27fa745baf8
alstn2468 <alstn2468_@naver.com>
2020-07-16 20:14:33 +0900

    [#31] Init loading overlay component, pagination component

---
+++ src/components/Pagination/PaginationProp.ts
@@ -0,0 +1,4 @@
+export type PaginationProp = {
+    currentPage: number;
+    range: number;
+};
---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { changeProductListCurrentPage } from 'actions/productListAction';
+import { PaginationProp } from 'components/Pagination/PaginationProp';
+
+const Pagination = ({ currentPage, range }: PaginationProp) => {
+    let pattern = null;
+
+    switch (true) {
+        case range < 7:
+            pattern = [...new Array(range)].map((_, i) => i + 1);
+            break;
+        case currentPage < 4:
+            pattern = [1, 2, 3, 4, 5, '...', range];
+            break;
+        case currentPage > range - 4:
+            pattern = [
+                1,
+                '...',
+                range - 4,
+                range - 3,
+                range - 2,
+                range - 1,
+                range,
+            ];
+            break;
+        default:
+            pattern = [
+                1,
+                '...',
+                currentPage - 1,
+                currentPage,
+                currentPage + 1,
+                '...',
+                range,
+            ];
+    }
+
+    return (
+        <div className="container">
+            <button
+                disabled={currentPage <= 1}
+                onClick={() =>
+                    useDispatch(changeProductListCurrentPage(currentPage - 1))
+                }
+            >
+                {'<'}
+            </button>
+            {pattern.map((label) => (
+                <button
+                    className={currentPage === label ? 'active' : ''}
+                    onClick={() =>
+                        useDispatch(changeProductListCurrentPage(label))
+                    }
+                >
+                    {label}
+                </button>
+            ))}
+            <button
+                disabled={currentPage >= range}
+                onClick={() =>
+                    useDispatch(changeProductListCurrentPage(currentPage + 1))
+                }
+            >
+                {'>'}
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
