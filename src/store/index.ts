623006e53ec8f19c6a0ca875d2c125771409a145
Kimminsu <alstn2468_@naver.com>
2020-07-16 10:01:02 +0900

    [#18] Install redux-thunk, redux-dev-tools and set up store with middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
