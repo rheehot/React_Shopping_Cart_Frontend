e9945e6c04b26aefb4a8218f1577a1d455dbfeed
Kimminsu <alstn2468_@naver.com>
2020-07-17 13:32:35 +0900

    [#42] Init cart route page

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+function Cart() {
+    return <div>CART</div>;
+}
+
+export default Cart;
--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from 'components/Home';
 import Header from 'components/Header';
 import Product from 'components/Product';
+import Cart from 'components/Cart';
 import Helemt from 'components/Helmet';
 
 function RootRouter(): React.ReactElement {
@@ -13,7 +14,7 @@ function RootRouter(): React.ReactElement {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/products" exact component={Product} />
-                <Route path="/cart" exact component={Home} />
+                <Route path="/cart" exact component={Cart} />
                 <Redirect path="*" to="/" />
             </Switch>
         </HashRouter>
